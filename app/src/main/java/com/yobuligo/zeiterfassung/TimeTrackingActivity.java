c3d594ff0a8ebfaacfd4b98e7b1b6214c9229cff
Peter <hoffmannpeter82@gmail.com>
2019-10-19 21:31:09 +0200

    Initial Commit

---
+++ app/src/main/java/com/yobuligo/zeiterfassung/TimeTrackingActivity.java
@@ -0,0 +1,14 @@
+package com.yobuligo.zeiterfassung;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimeTrackingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_tracking);
+    }
+}
