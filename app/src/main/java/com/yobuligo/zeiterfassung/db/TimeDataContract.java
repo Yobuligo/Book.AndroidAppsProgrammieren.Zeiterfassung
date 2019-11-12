4883a578086c378aef6f01f7723c5ec38ec92801
Peter <hoffmannpeter82@gmail.com>
2019-11-12 12:23:01 +0100

    Add TimeDateContract

---
+++ app/src/main/java/com/yobuligo/zeiterfassung/db/TimeDataContract.java
@@ -0,0 +1,61 @@
+package com.yobuligo.zeiterfassung.db;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class TimeDataContract {
+
+    // unique name within the operation system
+    public static final String AUTHORITY = "com.yobuligo.zeiterfassung.provider";
+
+    //base URI to access the content provider
+    public static final Uri AUTHORIZTY_URI = Uri.parse("content://" + AUTHORITY);
+
+    //contract for time
+    public static final class TimeData {
+
+        //sub folder to access the data / for me its more or less the entity
+        public static final String CONTENT_DIRECTORY = "time";
+
+        //URI to access the data
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORIZTY_URI, CONTENT_DIRECTORY);
+
+        //Typ for listing the data
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_DIRECTORY;
+
+        //Typ for a single date
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_DIRECTORY;
+
+        public interface Columns extends BaseColumns {
+            //Start time
+            String START_TIME = "start_time";
+
+            //End time
+            String END_TIME = "end_time";
+        }
+
+        public static final class Converter {
+            private static final String _ISO_8601_PATTERN = "yyyy-MM-dd'T'HH:mm";
+            public static final DateFormat DB_DATE_TIME_FORMATTER = new SimpleDateFormat(_ISO_8601_PATTERN, Locale.GERMANY);
+
+            public static Calendar parse(String dbTime) throws ParseException {
+                Calendar date = Calendar.getInstance();
+                date.setTime(DB_DATE_TIME_FORMATTER.parse(dbTime));
+                return date;
+            }
+
+            public static String format(Calendar dateTime) {
+                return DB_DATE_TIME_FORMATTER.format(dateTime.getTime());
+            }
+        }
+    }
+
+
+}
